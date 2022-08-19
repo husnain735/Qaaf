f7f556048b115daab699f3c99a5f09726c3cd216
bilalahmadsoft <111078475+bilalahmadsoft@users.noreply.github.com>
2022-08-19 20:57:29 +0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NgbCalendar, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  model: NgbDateStruct;
+  date: {year: number, month: number};
+
+  constructor(private calendar: NgbCalendar) {
+  }
+
+  selectToday() {
+    this.model = this.calendar.getToday();
+    
+  }
+
+}
---
+++ src/app/components/common-view/common-view.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CommonViewRoutingModule } from './common-view-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CommonViewRoutingModule
+  ]
+})
+export class CommonViewModule { }
