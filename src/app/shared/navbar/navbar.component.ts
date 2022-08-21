69b59ab02b7a72bcf14b644ed86ff3dc0be8c30e
bilalahmadsoft <111078475+bilalahmadsoft@users.noreply.github.com>
2022-08-21 17:32:26 +0500

    navabr setting

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../services/shared.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
+  tab: any = 'tab1';
+  
+  constructor(public _sharedService: SharedService) { }
+  
   ngOnInit(): void {
   }
-
+  onClick(check){
+    //    console.log(check);
+        if (check === 1){
+          this.tab = 'tab1';
+        }else if (check === 2){
+          this.tab = 'tab2';
+        }else if (check === 3){
+          this.tab = 'tab3';
+        }else if (check === 4){
+          this.tab = 'tab3';
+        }else if (check === 5){
+          this.tab = 'tab3';
+        }else if (check === 6){
+          this.tab = 'tab3';
+        }
+    }
 }
