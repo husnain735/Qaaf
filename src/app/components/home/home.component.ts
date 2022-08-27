21677e1d659ad9a3b9be8e04d7f598be477b2ad6
bilalahmadsoft <111078475+bilalahmadsoft@users.noreply.github.com>
2022-08-27 13:37:13 +0500

    owl-stage

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,7 +12,7 @@ export class HomeComponent implements OnInit {
   apiData: any[];
   customOptions: OwlOptions = {
     loop: true,
-    autoplay: true,
+    autoplay: false,
     center: true,
     dots: true,
     autoHeight: true,
