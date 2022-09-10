1a57ae0112ddbb7f36c4f52fc2c691a0e673a838
bilalahmadsoft <111078475+bilalahmadsoft@users.noreply.github.com>
2022-09-10 10:35:04 +0500

    whatsapp working

--- src/app/components/home/default/default.component.ts
+++ src/app/components/home/default/default.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
   selector: 'app-default',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefaultComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _sharedService: SharedService) { }
 
   ngOnInit(): void {
   }
